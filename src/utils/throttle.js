5519b89547bb46c53322ed68579d80fbb4d54e62
liwudi <liwudi@youxin.com>
2019-09-16 12:26:10 +0800

    添加throttle

--- src/utils/throttle.js
+++ src/utils/throttle.js
@@ -1,6 +1,23 @@
 /**
  * Created by mapbar_front on 2019-09-05.
  */
-export default function throttle () {
+export default function throttle (func, wait) {
+    let timer = null;
+    let previous;
+    return function () {
+        if (!previous) {
+            previous = new Date().getTime();
+        }
+        let remaining = wait - (new Date().getTime() - previous);
+        let rest = Array.prototype.slice.call(arguments);
 
+        if (remaining >= 0) {
+            if (timer) clearTimeout(timer);
+        } else {
+            timer = setTimeout(() => {
+                func.call(this, rest);
+                previous = new Date().getTime();
+            }, remaining);
+        }
+    }
 }
